ba9ee74274b1cae5e7179030071405ecbd4a8686
Sean Kelly <shonkelli@gmail.com>
2019-02-06 22:39:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,6 +5,7 @@ require 'yaml'
 def load_library(file_path)
   library = YAML.load_file(file_path)
   library = {"get_meaning" => {}, "get_emoticon" => {}}
+  binding.pry
 end
 
 def get_japanese_emoticon(file_path, emoticon)
